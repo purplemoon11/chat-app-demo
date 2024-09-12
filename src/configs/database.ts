5e4b69c77549c6357f5ca0d3d23ea764da9205da
Abishkar Karki <abishkarkarki@Abishkars-MacBook-Air.local>
2024-09-12 10:11:45 +0545

    feat: Changes on docker file

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
-import env from "./env";
 
 export const connectDB = async () => {
-  const mongoURI = env.dbName;
+  const mongoURI = process.env.MONGO_URI;
   if (!mongoURI) {
     throw new Error("MONGO_URI is not defined in environment variables");
   }
